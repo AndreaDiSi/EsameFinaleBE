6a04ac2b1949d9ddbe23a29a355b11bd138c9421
Andrea Di Simone <andreadisi14@gmail.com>
2026-06-10 17:26:08 +0200

    .

--- src/AutoConfig.Api/obj/Debug/net8.0/AutoConfig.Api.AssemblyInfo.cs
+++ src/AutoConfig.Api/obj/Debug/net8.0/AutoConfig.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoConfig.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3585c751de827d3dca8b7de24f133e20239dd340")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoConfig.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoConfig.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
